5240377519d9446bfdb00e5731da1c5f1ffbf234
Unknown <nkh.work1@gmail.com>
2018-07-04 14:29:36 -0500

    Uploaded project

---
+++ ATM/ATM.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ATM
+{
+    class ATM
+    {
+        private int hu;
+        private int ff;
+        private int tw;
+        private int te;
+        private int fv;
+        private int on;
+
+        public ATM()
+        {
+            hu = 10;
+            ff = 10;
+            tw = 10;
+            te = 10;
+            fv = 10;
+            on = 10;
+        }
+        
+
+
+        public void Restock()
+        {
+            hu = 10;
+            ff = 10;
+            tw = 10;
+            te = 10;
+            fv = 10;
+            on = 10;
+        }
+        public Boolean Withdraw(int amt)
+        {
+            int balance = (hu * 100) + (ff * 50) + (tw * 20) + (te * 10) + (fv * 5) + (on * 1);
+            if (balance >= amt)
+                
+            {
+                int disphu = amt / 100;
+                int dispff = (amt % 100) / 50;
+                int disptw = ((amt % 100) % 50) / 20;
+                int dispte = (((amt % 100) % 50) % 20) / 10;
+                int dispfv = ((((amt % 100) % 50) % 20) % 10) / 5;
+                int dispon = ((((amt % 100) % 50) % 20) % 10) % 5;
+                hu -= disphu;
+                ff -= dispff;
+                tw -= disptw;
+                te -= dispte;
+                fv -= dispfv;
+                on -= dispon; 
+
+                return true;
+            }
+            return false;
+        }
+
+        public void Denominations(List<int> bills)
+        {
+
+            int i = 0;
+            for (i = 0; i < bills.Count; i++)
+            {
+                if (bills[i] == 1)
+                {
+                    Console.WriteLine("$1 - " + on);
+                }
+
+                if (bills[i] == 5)
+                {
+                    Console.WriteLine("$5 - " + fv);
+                }
+
+                if (bills[i] == 10)
+                {
+                    Console.WriteLine("$10 - " + te);
+                }
+
+                if (bills[i] == 20)
+                {
+                    Console.WriteLine("$20: " + tw);
+                }
+
+                if (bills[i] == 50)
+                {
+                    Console.WriteLine("$50: " + ff);
+                }
+
+                if (bills[i] == 100)
+                {
+                    Console.WriteLine("$100: " + hu);                   
+                }
+            }
+        }
+    }
+}
---
+++ ATM/ATMoperator.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ATM
+{
+    class ATMoperator
+    {
+        static void Main(string[] args)
+        {
+            string input = "input";
+            char command = 'a';
+            ATM atm = new ATM();
+            Console.WriteLine("Welcome to the ATM.");           
+                Console.WriteLine("Options:");
+                Console.WriteLine("1. Withdraw - Press W plus amount in the form 'W $x'");
+                Console.WriteLine("2. Display Balance: Press I followed by the bills that you wish to check in the format I $x $y $z");
+                Console.WriteLine("3. Restock the machine: Press R");
+                Console.WriteLine("4. Quit: Press Q");
+
+            do
+            {
+                input = Console.ReadLine();
+
+                try
+                {
+                    command = input[0];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Error: Invalid command");
+                    continue;
+                }
+
+                if (command.Equals('W') || command.Equals('I') || command.Equals('R') || command.Equals('Q'))
+                {
+                    switch (command)
+                    {
+                        case 'W':
+                            string temp = input.Substring(input.IndexOf("$") + 1);
+                            int amt = 0;
+                            try
+                            {
+                                amt = int.Parse(temp);
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Error: Invalid command");
+                                break;
+                            }
+                            bool success = atm.Withdraw(amt);
+                            if (success)
+                            {
+                                Console.WriteLine("Success: Dispensed " + "$" + amt);
+                                Console.WriteLine();
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: Insufficient Funds");
+                                Console.WriteLine();
+                            }
+                            break;
+
+                        case 'I':
+                            int i = 0;
+                            List<string> sDenominations = new List<string>();
+                            StringBuilder d = new StringBuilder();
+                            string s = "";
+                            for (i = 0; i < input.Length; i++)
+                            {
+                                if (char.IsNumber(input[i]))
+                                {
+                                    d.Append(input[i]);
+                                }
+                                if (input[i] == ' ')
+                                {
+                                    s = d.ToString();
+                                    sDenominations.Add(s);
+                                    d = new StringBuilder();
+                                }
+                            }
+                            s = d.ToString();
+                            sDenominations.Add(s);
+                            List<int> bills = new List<int>();
+                            for (i = 1; i < sDenominations.Count(); i++)
+                            {
+                                try
+                                {
+                                    bills.Add(int.Parse(sDenominations[i]));
+                                }
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("Error: Invalid command");
+                                }
+                            }
+                            atm.Denominations(bills);
+                            break;
+
+                        case 'R':
+                            atm.Restock();
+                            Console.WriteLine("Restocked Machine");
+                            break;
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Error: Invalid command");
+                    Console.WriteLine();
+                }
+            }
+            while (command != 'Q');
+            Console.WriteLine("Thank you for using the ATM. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
